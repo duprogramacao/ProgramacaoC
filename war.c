19fbc06ffaf5cc5cbbca816ec0c61aefc26ee99f
duprogramacao <duabreu27@gmail.com>
2025-09-06 17:00:11 +0000

    // Fiz os loops com scans para registrar os // territórios

--- war.c
+++ war.c
@@ -2,110 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TAM_STRING 100
+#define TAMANHO 5
+#define NOME_MAX_TAM 30
+#define COR_MAX_TAM 10
 
-struct Territorio{
-    char nome[30];
-    char cor[10];
-    int tropas;
-};
+typedef struct {
+    char **nome;
+    char **cor;
+    int *tropas;
+} Territorio;
 
 void limparBufferEntrada(){
     int y;
     while ((y = getchar()) != '\n' && y != EOF);
-}
+};
+
 
 int main(){
-    struct Territorio a;
-    printf("Vamos cadastrar os 5 territórios iniciais do nosso mundo.\n");
-    printf("---  Cadastrando território 1  ---\n");
-    printf("Nome do território:");
-    fgets(a.nome, sizeof a.nome, stdin);
+    Territorio* p = (Territorio*)malloc(sizeof(Territorio));
+    if (p == NULL) {
+        printf("Falha ao alocar a memória.\n");
+        return 1;
+    }
+
+    p->nome = (char**)malloc(TAMANHO * sizeof(char*));  
+    p->cor = (char**)malloc(TAMANHO * sizeof(char*));   
+    p->tropas = (int*)malloc(TAMANHO * sizeof(int));
+
+
+    if (p->nome == NULL || p->cor == NULL || p->tropas == NULL) {
+        printf("Falha ao alocar memória para os dados.\n");
+        free(p);
+        return 1;
+    }
+
+    for (int i = 0; i < TAMANHO; i++){
+        p->nome[i] = (char*)malloc(NOME_MAX_TAM * sizeof(char));
+        p->cor[i] = (char*)malloc(COR_MAX_TAM * sizeof(char));
+
+        if (p->nome[i] == NULL || p->cor[i] == NULL){
+            printf("Falha ao inserir memória para cor ou nome.\n");
+        }
+        
+
+    printf("Insira o nome do território %d: ", i + 1);
+    scanf("%s", p->nome[i]);
     
-    printf("Cor do exército (ex.: Azul, Verde, etc.): ");
-    fgets(a.cor, sizeof a.cor, stdin);
+    printf("Insira a cor do território %d (ex.: Azul, Verde, etc.): ", i +1);
+    scanf("%s", p->cor[i]);
 
-    printf("Número de tropas: ");
-    scanf("%d", &a.tropas);
+    printf("Insira o número de tropas do território %d: ", i + 1);
+    scanf("%d", p->tropas);
+    }
 
-    limparBufferEntrada();
+    printf("\nTerritórios cadastrados:\n");
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("Território %d - Nome: %s, Cor: %s\n", i + 1, p->nome[i], p->cor[i]);
+    }
+    
 
-    struct Territorio b;
-    printf("---  Cadastrando território 2  ---\n");
-    printf("Nome do território:");
-    fgets(b.nome, sizeof b.nome, stdin);
 
-    printf("Cor do exército (ex.: Azul, Verde, etc.):");
-    fgets(b.cor, sizeof b.cor, stdin);
-
-    printf("Número de tropas:");
-    scanf("%d", &b.tropas);
-
-    limparBufferEntrada();
-
-    struct Territorio c;
-    printf("---  Cadastrando território 3  ---\n");
-    printf("Nome do território:");
-    fgets(c.nome, sizeof c.nome, stdin);
-
-    printf("Cor do exército (ex.: Azul, Verde, etc.):");
-    fgets(c.cor, sizeof c.cor, stdin);
-
-    printf("Número de tropas:");
-    scanf("%d", &c.tropas);
-
-    limparBufferEntrada();
-
-    struct Territorio d;
-    printf("---  Cadastrando território 4  ---\n");
-    printf("Nome do território:");
-    fgets(d.nome, sizeof d.nome, stdin);
-
-    printf("Cor do exército (ex.: Azul, Verde, etc.):");
-    fgets(d.cor, sizeof d.cor, stdin);
-
-    printf("Número de tropas:");
-    scanf("%d", &d.tropas);
-
-    limparBufferEntrada();
-
-    struct Territorio e;
-    printf("---  Cadastrando território 5  ---\n");
-    printf("Nome do território:");
-    fgets(e.nome, sizeof e.nome, stdin);
-
-    printf("Cor do exército (ex.: Azul, Verde, etc.):");
-    fgets(e.cor, sizeof e.cor, stdin);
-
-    printf("Número de tropas:");
-    scanf("%d", &e.tropas);
-
-    limparBufferEntrada();
-
-    printf("TERRITÓRIO 1");
-    printf(" - Nome: %s", a.nome);
-    printf(" - Dominado por: %s", a.cor);
-    printf(" - Tropas: %d\n", a.tropas);
-
-    printf("TERRITÓRIO 2");
-    printf(" - Nome: %s", b.nome);
-    printf(" - Dominado por: %s", b.cor);
-    printf(" - Tropas: %d\n", b.tropas);
-
-    printf("TERRITÓRIO 3");
-    printf(" - Nome: %s", c.nome);
-    printf(" - Dominado por: %s", c.cor);
-    printf(" - Tropas: %d\n", c.tropas);
-
-    printf("TERRITÓRIO 4");
-    printf(" - Nome: %s", d.nome);
-    printf(" - Dominado por: %s", d.cor);
-    printf(" - Tropas: %d\n", d.tropas);
-
-    printf("TERRITÓRIO 5");
-    printf(" - Nome: %s", e.nome);
-    printf(" - Dominado por: %s", e.cor);
-    printf(" - Tropas: %d\n", e.tropas);
 
 
 }
